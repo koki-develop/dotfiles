2fd485a5653301f09fc3beaea3de30d2032b19c0
koki-develop <kou.pg.0131@gmail.com>
2022-02-21 15:24:49 +0900

    selectionColor を設定

--- src/hyper/hyper.js
+++ src/hyper/hyper.js
@@ -2,7 +2,8 @@ module.exports = {
   config: {
     fontSize: 16,
     fontFamily: '"Ricty Diminished"',
-    cursorColor: "#ffffff",
+    cursorColor: "rgba(255, 255, 255, 0.5)",
+    selectionColor: "rgba(255, 255, 255, 0.5)",
 
     summon: {
       hotkey: "Option+Space",
