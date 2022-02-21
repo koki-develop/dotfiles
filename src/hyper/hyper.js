d8130e7d00119196687dd75e6f4fa01295f0a2ef
koki-develop <kou.pg.0131@gmail.com>
2022-02-21 17:05:16 +0900

    文字色を lightGray にした

--- src/hyper/hyper.js
+++ src/hyper/hyper.js
@@ -1,13 +1,19 @@
 module.exports = {
   config: {
+    // font
     fontSize: 16,
     fontFamily: '"Ricty Diminished"',
+
+    // color
+    foregroundColor: "lightGray",
     cursorColor: "#ff0000",
     selectionColor: "rgba(255, 255, 255, 0.5)",
 
+    // hyperterm-summon
     summon: {
       hotkey: "Option+Space",
     },
   },
+
   plugins: ["hyperpower", "hyperterm-summon"],
 };
