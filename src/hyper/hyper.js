2319e1ea0bf3be47c21c0926177ffada4fbe3ad0
koki-develop <kou.pg.0131@gmail.com>
2022-02-22 21:41:48 +0900

    hyper-statusbar の設定を追加

--- src/hyper/hyper.js
+++ src/hyper/hyper.js
@@ -8,12 +8,19 @@ module.exports = {
     foregroundColor: "lightGray",
     cursorColor: "#ff0000",
     selectionColor: "rgba(255, 255, 255, 0.5)",
+    borderColor: "#333",
 
     // hyperterm-summon
     summon: {
       hotkey: "Option+Space",
     },
+
+    // hyper-statusbar
+    statusbar: {
+      panels: ["battery", "user", "ip", "clock"],
+    },
   },
 
   plugins: ["hyperpower", "hyperterm-summon"],
+  localPlugins: ["hyper-statusbar"],
 };
