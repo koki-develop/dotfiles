ae4d479478a1cf001c2a66d9f9c285aa5d640e39
koki-develop <kou.pg.0131@gmail.com>
2022-05-03 20:23:46 +0900

    hammerspoon の設定ファイルを作成するコマンドを追記

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,3 +8,6 @@ ln -sf ~/dotfiles/src/git/config ~/.gitconfig
 ln -sf ~/dotfiles/src/git/ignore ~/.config/git/ignore
 
 ln -sf ~/dotfiles/src/hyper/hyper.js ~/.hyper.js
+
+mkdir -p ~/.hammerspoon
+ln -sf ~/dotfiles/src/hammerspoon/init.lua ~/.hammerspoon/init.lua
