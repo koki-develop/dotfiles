20313b8c8d7120fee71709ab3e3a80e06ae50a76
koki-develop <kou.pg.0131@gmail.com>
2022-06-24 07:45:22 +0900

    vscode, chrome をすぐ開けるようにした

--- src/hammerspoon/init.lua
+++ src/hammerspoon/init.lua
@@ -2,3 +2,13 @@ hs.hotkey.bind({"option"}, "space", function()
   local hyper = hs.application.find('hyper')
   hs.application.launchOrFocus("/Applications/Hyper.app")
 end)
+
+hs.hotkey.bind({"option"}, "v", function()
+  local hyper = hs.application.find('hyper')
+  hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+end)
+
+hs.hotkey.bind({"option"}, "c", function()
+  local hyper = hs.application.find('hyper')
+  hs.application.launchOrFocus("/Applications/Google Chrome.app")
+end)
