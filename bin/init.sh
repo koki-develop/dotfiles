47633c2649cd0fef2982156497e5cf42aabf9a0f
koki-develop <kou.pg.0131@gmail.com>
2023-07-13 11:15:13 +0900

    Add script to install latest HackGen font

---
+++ bin/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+#
+# Install HackGen
+#
+
+# Get latest version
+HACK_GEN_REPO=yuru7/HackGen
+HACK_GEN_VERSION=$(curl -s https://api.github.com/repos/${HACK_GEN_REPO}/releases/latest | jq -r .tag_name)
+
+# Download and install
+HACK_GEN_DOWNLOAD_URL=https://github.com/yuru7/HackGen/releases/download/${HACK_GEN_VERSION}/HackGen_NF_${HACK_GEN_VERSION}.zip
+wget -O /tmp/HackGen.zip "${HACK_GEN_DOWNLOAD_URL}"
+unzip -o /tmp/HackGen.zip -d /tmp
+cp /tmp/HackGen_NF_"${HACK_GEN_VERSION}"/HackGenConsoleNF-* "${HOME}"/Library/Fonts/
