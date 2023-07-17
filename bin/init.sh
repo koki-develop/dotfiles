31fd7d39f5139dcc93505edece901837e19c7c45
koki-develop <kou.pg.0131@gmail.com>
2023-07-17 22:45:51 +0900

    Add git-secrets setup to init.sh script

--- bin/init.sh
+++ bin/init.sh
@@ -71,3 +71,10 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +PlugInstall +UpdateRemotePlugins +qall
+
+#
+# Set up git-secrets
+#
+
+git secrets --register-aws --global
+git secrets --install ~/.git-templates/git-secrets
