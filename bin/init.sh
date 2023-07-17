1414b8ea514f682d04face21f2c18d7e3cc2da8c
koki-develop <kou.pg.0131@gmail.com>
2023-07-17 22:45:42 +0900

    Add vim-plug installation to init.sh script

--- bin/init.sh
+++ bin/init.sh
@@ -64,3 +64,10 @@ cp /tmp/HackGen_NF_"${HACK_GEN_VERSION}"/HackGenConsoleNF-* "${HOME}"/Library/Fo
 #
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+#
+# Install vim-plug
+#
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall +UpdateRemotePlugins +qall
