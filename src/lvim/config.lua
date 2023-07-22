9d15040880a86c49d36d332f88766279f653fc58
koki-develop <kou.pg.0131@gmail.com>
2023-07-22 15:40:07 +0900

    Add settings for GitHub Copilot

--- src/lvim/config.lua
+++ src/lvim/config.lua
@@ -5,5 +5,25 @@
 
 lvim.format_on_save.enabled = true
 
+-- Angular
 -- https://www.lunarvim.org/docs/features/supported-frameworks/angular
 require("lvim.lsp.manager").setup("angularls")
+
+-- GitHub Copilot
+-- https://github.com/LunarVim/LunarVim/issues/1856#issuecomment-954224770
+lvim.plugins = {
+  { "github/copilot.vim" }
+}
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+local cmp = require "cmp"
+lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
+  cmp.mapping.abort()
+  local copilot_keys = vim.fn["copilot#Accept"]()
+  if copilot_keys ~= "" then
+    vim.api.nvim_feedkeys(copilot_keys, "i", true)
+  else
+    fallback()
+  end
+end
