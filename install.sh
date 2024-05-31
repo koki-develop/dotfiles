79c5a499c78f4e7cab0cf6329c27519245e40437
koki sato <koki@kokinoMacBook-Air.local>
2024-05-31 22:15:27 +0900

    Add Homebrew and Dashlane CLI install to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install dashlane/tap/dashlane-cli
+dcli sync
+
 #
 # chezmoi
 # https://www.chezmoi.io/
