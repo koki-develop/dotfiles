ef3c19e8395e92f21260fe0a156eb2b08e3b6c52
koki-develop <kou.pg.0131@gmail.com>
2024-06-03 17:14:52 +0900

    <commit message>Remove HackGen installation script

--- scripts/run_once_after_01_install_tools.sh
+++ scripts/run_once_after_01_install_tools.sh
@@ -12,25 +12,6 @@ brew bundle --global
 echo "Done."
 
 #
-# HackGen
-# https://github.com/yuru7/HackGen
-#
-
-echo "Installing HackGen..."
-
-# Get latest version
-HACK_GEN_REPO=yuru7/HackGen
-HACK_GEN_VERSION=$(curl -s https://api.github.com/repos/${HACK_GEN_REPO}/releases/latest | jq -r .tag_name)
-
-# Download and install
-HACK_GEN_DOWNLOAD_URL=https://github.com/${HACK_GEN_REPO}/releases/download/${HACK_GEN_VERSION}/HackGen_NF_${HACK_GEN_VERSION}.zip
-wget -O /tmp/HackGen.zip "${HACK_GEN_DOWNLOAD_URL}"
-unzip -o /tmp/HackGen.zip -d /tmp
-cp /tmp/HackGen_NF_"${HACK_GEN_VERSION}"/HackGenConsoleNF-* "${HOME}"/Library/Fonts/
-
-echo "Done."
-
-#
 # zplug
 # https://github.com/zplug/zplug#installation
 #
