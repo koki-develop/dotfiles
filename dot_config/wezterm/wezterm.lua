3fed851e2f7e5fba8905658ba81fc0d1a6d02c80
koki-develop <kou.pg.0131@gmail.com>
2024-06-05 22:08:37 +0900

    Refactor wezterm.lua to use config_builder()

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,51 +1,56 @@
 local wezterm = require 'wezterm';
 
-return {
-  font = wezterm.font("HackGen Console NF"),
-  font_size = 16.0,
+local config = wezterm.config_builder()
 
-  color_scheme = "Monokai Remastered",
-  colors = {
-    cursor_bg = 'white',
+config.font = wezterm.font("HackGen Console NF")
+config.font_size = 16.0
+
+config.color_scheme = "Monokai Remastered"
+config.colors = {
+  cursor_bg = 'white',
+}
+
+config.use_ime = true
+
+config.adjust_window_size_when_changing_font_size = false
+
+config.send_composed_key_when_left_alt_is_pressed = true
+
+config.keys = {
+  -- Screen
+  {
+    key = 'f',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.ToggleFullScreen,
   },
-  use_ime = true,
-  adjust_window_size_when_changing_font_size = false,
-  send_composed_key_when_left_alt_is_pressed = true,
-
-  keys = {
-    -- Screen
-    {
-      key = 'f',
-      mods = 'CMD|CTRL',
-      action = wezterm.action.ToggleFullScreen,
-    },
-    -- Close Pane
-    {
-      key = 'w',
-      mods = 'CMD',
-      action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
-    -- Select Pane
-    {
-      key = '[',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection 'Prev'
-    },
-    {
-      key = ']',
-      mods = 'CMD',
-      action = wezterm.action.ActivatePaneDirection 'Next'
-    },
-    -- Split Window
-    {
-      key = 'd',
-      mods = 'CMD',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'D',
-      mods = 'CMD',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
+  -- Close Pane
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  -- Select Pane
+  {
+    key = '[',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection 'Prev'
+  },
+  {
+    key = ']',
+    mods = 'CMD',
+    action = wezterm.action.ActivatePaneDirection 'Next'
+  },
+  -- Split Window
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'D',
+    mods = 'CMD',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
 }
+
+return config
