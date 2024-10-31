3df8db4822f133bfb95f482c24a9cb4ee9c763fd
koki-develop <kou.pg.0131@gmail.com>
2024-10-31 09:50:04 +0900

    Add keybindings to match Terminal.app behavior

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm';
+local act = wezterm.action;
 
 local config = wezterm.config_builder()
 
@@ -50,6 +51,20 @@ config.keys = {
     key = 'D',
     mods = 'CMD',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' },
   },
 }
 
