e70eaeb13ebb67f87f2da27d48eca5271b54512e
koki-develop <kou.pg.0131@gmail.com>
2025-01-08 14:16:49 +0900

    add window padding and background opacity settings

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -15,6 +15,15 @@ config.use_ime = true
 
 config.adjust_window_size_when_changing_font_size = false
 
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+config.window_background_opacity = 0.9
+
 config.send_composed_key_when_left_alt_is_pressed = true
 
 config.keys = {
