6b1bd6814538336ac7f0853d8f68f2fc89f777f8
koki-develop <kou.pg.0131@gmail.com>
2025-06-16 22:47:23 +0900

    Update wezterm configuration

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -26,41 +26,9 @@ config.window_background_opacity = 0.9
 
 config.send_composed_key_when_left_alt_is_pressed = true
 
+config.enable_tab_bar = false
+
 config.keys = {
-  -- Screen
-  {
-    key = 'f',
-    mods = 'CMD|CTRL',
-    action = wezterm.action.ToggleFullScreen,
-  },
-  -- Close Pane
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
-  -- Select Pane
-  {
-    key = '[',
-    mods = 'CMD',
-    action = wezterm.action.ActivatePaneDirection 'Prev'
-  },
-  {
-    key = ']',
-    mods = 'CMD',
-    action = wezterm.action.ActivatePaneDirection 'Next'
-  },
-  -- Split Window
-  {
-    key = 'd',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'D',
-    mods = 'CMD',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
   -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
   {
     key = 'LeftArrow',
