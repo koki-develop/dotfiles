fcbfb36b328d4c0830cf5e7ce0249e3630aad077
koki-develop <kou.pg.0131@gmail.com>
2025-09-25 22:04:34 +0900

    nvim -> lvim

--- run_once_after_01_install_tools.sh
+++ run_once_after_01_install_tools.sh
@@ -23,18 +23,6 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 echo "Done."
 
 #
-# vim-plug
-# https://github.com/junegunn/vim-plug#unix-linux
-#
-
-echo "Installing vim-plug..."
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim +PlugInstall +UpdateRemotePlugins +qall
-
-echo "Done."
-
-#
 # xbar-plugin-github
 # https://github.com/koki-develop/xbar-plugin-github
 #
