1aa9e387be0224653bcecdf6193be502b2ec5885
koki-develop <kou.pg.0131@gmail.com>
2025-09-28 09:05:14 +0900

    Create lvim/config.lua

---
+++ dot_config/lvim/config.lua
@@ -0,0 +1,6 @@
+--
+-- https://www.lunarvim.org/docs/configuration
+--
+
+-- https://www.lunarvim.org/docs/configuration/language-features/linting-and-formatting#formatting-on-save
+lvim.format_on_save.enabled = true
