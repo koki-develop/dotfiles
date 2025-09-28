fd8aa942820c936d8fb832665da50f8d8987ed04
koki-develop <kou.pg.0131@gmail.com>
2025-09-28 10:06:38 +0900

    Add configuration for lualine

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -1,6 +1,12 @@
---
 -- https://www.lunarvim.org/docs/configuration
---
 
 -- https://www.lunarvim.org/docs/configuration/language-features/linting-and-formatting#formatting-on-save
 lvim.format_on_save.enabled = true
+
+-- https://www.lunarvim.org/docs/configuration/appearance/statusline
+lvim.builtin.lualine.on_config_done = function(lualine)
+  local config = lualine.get_config()
+  config.sections.lualine_c = {
+    { "filename", path = 1 } }
+  lualine.setup(config)
+end
